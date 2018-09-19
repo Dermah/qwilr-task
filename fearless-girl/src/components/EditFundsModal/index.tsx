07b15fc642bc1291b3a3550a30cae85c9609fd68
Sputnik <inquiry@dermah.com>
2018-09-19 22:14:11 +1000

    Add actions and text to funds modal

--- fearless-girl/src/components/EditFundsModal/index.tsx
+++ fearless-girl/src/components/EditFundsModal/index.tsx
@@ -3,10 +3,18 @@ import * as React from "react";
 import injectSheet, { WithSheet } from "react-jss";
 import { compose } from "recompose";
 
+import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
-const styles = {};
+const styles = {
+  actionBar: {
+    justifyContent: "space-between"
+  }
+};
 
 interface Props {
   open: boolean;
@@ -16,6 +24,16 @@ interface InnerProps extends WithSheet<typeof styles>, Props {}
 const Page = ({ classes, open }: InnerProps) => (
   <Dialog open={true}>
     <DialogTitle>Modify funds</DialogTitle>
+    <DialogContent>
+      <DialogContentText>
+        Add or remove funds from your account.
+      </DialogContentText>
+      <DialogContentText>You currently have funds</DialogContentText>
+    </DialogContent>
+    <DialogActions className={classes.actionBar}>
+      <Button>Remove</Button>
+      <Button color="primary">Add</Button>
+    </DialogActions>
   </Dialog>
 );
 
