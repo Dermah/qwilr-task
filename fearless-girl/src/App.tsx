9bce2cd0c7da06c75e6ab379e06426a5e378f7f6
Sputnik <inquiry@dermah.com>
2018-09-19 21:56:53 +1000

    Use simple page component

--- fearless-girl/src/App.tsx
+++ fearless-girl/src/App.tsx
@@ -3,6 +3,11 @@ import * as React from "react";
 import ApolloClient, { gql } from "apollo-boost";
 import { ApolloProvider, Query } from "react-apollo";
 
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+
+import Page from "./components/Page";
+
 const client = new ApolloClient({
   uri: "http://192.168.1.4:4000"
 });
@@ -11,7 +16,7 @@ class App extends React.Component {
   public render() {
     return (
       <ApolloProvider client={client}>
-        <div>
+        <Page>
           <Query
             query={gql`
               {
@@ -22,18 +27,20 @@ class App extends React.Component {
             `}
           >
             {({ loading, error, data }) => (
-              <p className="App-intro">
-                Did you know you have{" "}
-                {(data &&
-                  data.viewer &&
-                  typeof data.viewer.cash === "number" &&
-                  data.viewer.cash.toString()) ||
-                  "some"}{" "}
-                dollarydoos?
-              </p>
+              <Card>
+                <CardContent>
+                  Did you know you have{" "}
+                  {(data &&
+                    data.viewer &&
+                    typeof data.viewer.cash === "number" &&
+                    data.viewer.cash.toString()) ||
+                    "some"}{" "}
+                  dollarydoos?
+                </CardContent>
+              </Card>
             )}
           </Query>
-        </div>
+        </Page>
       </ApolloProvider>
     );
   }
