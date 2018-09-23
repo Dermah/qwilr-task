2e0895c417f0d37a03556e5db1686b40c5c8a160
Sputnik <inquiry@dermah.com>
2018-09-23 23:10:41 +1000

    Show stock price on cards

--- fearless-girl/src/components/StockCard/index.tsx
+++ fearless-girl/src/components/StockCard/index.tsx
@@ -8,6 +8,11 @@ const styles: Styles = {
   card: {
     margin: "1em",
     width: "250px"
+  },
+  header: {
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "space-between"
   }
 };
 
@@ -20,9 +25,12 @@ interface InnerProps extends WithSheet<typeof styles>, Props {}
 const StockCard = ({ classes, stock }: InnerProps) => (
   <Card className={classes.card}>
     <CardContent>
-      <Typography variant="headline" component="h2">
-        {stock.id}
-      </Typography>
+      <div className={classes.header}>
+        <Typography variant="headline" component="h2">
+          {stock.id}
+        </Typography>
+        <Typography variant="subheading">${stock.latestPrice}</Typography>
+      </div>
       <Typography variant="subheading" component="h3">
         {stock.name}
       </Typography>
